6ad540ed1e488f4d522e1ccba7ff9c08173f86c3
jingtalk <jinghuang1130@yahoo.com>
2016-12-30 20:06:10 +0800

    update hook

---
+++ hooktest/src/main/java/com/example/jinghuang/hooktest/HookBinderActivity.java
@@ -0,0 +1,13 @@
+package com.example.jinghuang.hooktest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class HookBinderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hook_binder);
+    }
+}
---
+++ hooktest/src/main/java/com/example/jinghuang/hooktest/MyActivity.java
@@ -0,0 +1,10 @@
+package com.example.jinghuang.hooktest;
+
+import android.app.Activity;
+
+/**
+ * Created by hakimhuang on 2016/12/30.
+ */
+
+public class MyActivity extends Activity {
+}
